87fb4f3acbae28dcf55357314db1a39dba66283f
Kenchi Chen <david92219@gmail.com>
2021-06-28 13:11:51 +0800

    [功能] 設定 c icon 啟用設定

---
+++ src/plugins/guideline/icon.js
@@ -0,0 +1,364 @@
+export default function() {
+  return function({ addUtilities, variants }) {
+    addUtilities(
+      {
+        '.icon-add::before': {
+          content: '"\\e900"',
+        },
+
+        '.icon-address::before': {
+          content: '"\\e901"',
+        },
+
+        '.icon-annex::before': {
+          content: '"\\e902"',
+        },
+
+        '.icon-arrow-down::before': {
+          content: '"\\e903"',
+        },
+
+        '.icon-arrow-left::before': {
+          content: '"\\e904"',
+        },
+
+        '.icon-arrow-right::before': {
+          content: '"\\e905"',
+        },
+
+        '.icon-arrow-up::before': {
+          content: '"\\e906"',
+        },
+
+        '.icon-back::before': {
+          content: '"\\e907"',
+        },
+
+        '.icon-badge::before': {
+          content: '"\\e908"',
+        },
+
+        '.icon-bell::before': {
+          content: '"\\e909"',
+        },
+
+        '.icon-broadcast::before': {
+          content: '"\\e90a"',
+        },
+
+        '.icon-camera-filled::before': {
+          content: '"\\e90b"',
+        },
+
+        '.icon-candidate::before': {
+          content: '"\\e90c"',
+        },
+
+        '.icon-check::before': {
+          content: '"\\e90d"',
+        },
+
+        '.icon-ci::before': {
+          content: '"\\e90e"',
+        },
+
+        '.icon-circle-alert::before': {
+          content: '"\\e90f"',
+        },
+
+        '.icon-circle-behance::before': {
+          content: '"\\e910"',
+        },
+
+        '.icon-circle-blogger::before': {
+          content: '"\\e911"',
+        },
+
+        '.icon-circle-dribbble::before': {
+          content: '"\\e912"',
+        },
+
+        '.icon-circle-fb::before': {
+          content: '"\\e913"',
+        },
+
+        '.icon-circle-github::before': {
+          content: '"\\e914"',
+        },
+
+        '.icon-circle-go-top::before': {
+          content: '"\\e915"',
+        },
+
+        '.icon-circle-heart::before': {
+          content: '"\\e916"',
+        },
+
+        '.icon-circle-instegream::before': {
+          content: '"\\e917"',
+        },
+
+        '.icon-circle-line::before': {
+          content: '"\\e918"',
+        },
+
+        '.icon-circle-link::before': {
+          content: '"\\e919"',
+        },
+
+        '.icon-circle-linkedin::before': {
+          content: '"\\e91a"',
+        },
+
+        '.icon-circle-mail::before': {
+          content: '"\\e91b"',
+        },
+
+        '.icon-circle-medium::before': {
+          content: '"\\e91c"',
+        },
+
+        '.icon-circle-outlook::before': {
+          content: '"\\e91d"',
+        },
+
+        '.icon-circle-profession::before': {
+          content: '"\\e91e"',
+        },
+
+        '.icon-circle-report::before': {
+          content: '"\\e91f"',
+        },
+
+        '.icon-circle-share::before': {
+          content: '"\\e920"',
+        },
+
+        '.icon-circle-stackoverflow::before': {
+          content: '"\\e921"',
+        },
+
+        '.icon-circle-twitter::before': {
+          content: '"\\e922"',
+        },
+
+        '.icon-circle-youtube::before': {
+          content: '"\\e923"',
+        },
+
+        '.icon-clear::before': {
+          content: '"\\e924"',
+        },
+
+        '.icon-company::before': {
+          content: '"\\e925"',
+        },
+
+        '.icon-copy::before': {
+          content: '"\\e926"',
+        },
+
+        '.icon-delete::before': {
+          content: '"\\e927"',
+        },
+
+        '.icon-desktop::before': {
+          content: '"\\e928"',
+        },
+
+        '.icon-desktop2::before': {
+          content: '"\\e929"',
+        },
+
+        '.icon-drag::before': {
+          content: '"\\e92a"',
+        },
+
+        '.icon-draw::before': {
+          content: '"\\e92b"',
+        },
+
+        '.icon-edit::before': {
+          content: '"\\e92c"',
+        },
+
+        '.icon-extend::before': {
+          content: '"\\e92d"',
+        },
+
+        '.icon-eye::before': {
+          content: '"\\e92e"',
+        },
+
+        '.icon-file::before': {
+          content: '"\\e92f"',
+        },
+
+        '.icon-gift-filled::before': {
+          content: '"\\e930"',
+        },
+
+        '.icon-gift::before': {
+          content: '"\\e931"',
+        },
+
+        '.icon-go-top::before': {
+          content: '"\\e932"',
+        },
+
+        '.icon-heart-filled::before': {
+          content: '"\\e933"',
+        },
+
+        '.icon-heart::before': {
+          content: '"\\e934"',
+        },
+
+        '.icon-history::before': {
+          content: '"\\e935"',
+        },
+
+        '.icon-home-filled::before': {
+          content: '"\\e936"',
+        },
+
+        '.icon-home::before': {
+          content: '"\\e937"',
+        },
+
+        '.icon-idea::before': {
+          content: '"\\e938"',
+        },
+
+        '.icon-image::before': {
+          content: '"\\e939"',
+        },
+
+        '.icon-info::before': {
+          content: '"\\e93a"',
+        },
+
+        '.icon-intro::before': {
+          content: '"\\e93b"',
+        },
+
+        '.icon-jobs::before': {
+          content: '"\\e93c"',
+        },
+
+        '.icon-layout::before': {
+          content: '"\\e93d"',
+        },
+
+        '.icon-letter::before': {
+          content: '"\\e93e"',
+        },
+
+        '.icon-lock::before': {
+          content: '"\\e93f"',
+        },
+
+        '.icon-mail-filled::before': {
+          content: '"\\e940"',
+        },
+
+        '.icon-mail::before': {
+          content: '"\\e941"',
+        },
+
+        '.icon-map::before': {
+          content: '"\\e942"',
+        },
+
+        '.icon-menu::before': {
+          content: '"\\e943"',
+        },
+
+        '.icon-minus::before': {
+          content: '"\\e944"',
+        },
+
+        '.icon-mobile::before': {
+          content: '"\\e945"',
+        },
+
+        '.icon-mobile2::before': {
+          content: '"\\e946"',
+        },
+
+        '.icon-more::before': {
+          content: '"\\e947"',
+        },
+
+        '.icon-notification-filled::before': {
+          content: '"\\e948"',
+        },
+
+        '.icon-notification::before': {
+          content: '"\\e949"',
+        },
+
+        '.icon-opinion::before': {
+          content: '"\\e94a"',
+        },
+
+        '.icon-package::before': {
+          content: '"\\e94b"',
+        },
+
+        '.icon-picture::before': {
+          content: '"\\e94c"',
+        },
+
+        '.icon-pin-filled::before': {
+          content: '"\\e94d"',
+        },
+
+        '.icon-referee::before': {
+          content: '"\\e94e"',
+        },
+
+        '.icon-search::before': {
+          content: '"\\e94f"',
+        },
+
+        '.icon-share::before': {
+          content: '"\\e950"',
+        },
+
+        '.icon-star-filled::before': {
+          content: '"\\e951"',
+        },
+
+        '.icon-star::before': {
+          content: '"\\e952"',
+        },
+
+        '.icon-star2::before': {
+          content: '"\\e953"',
+        },
+
+        '.icon-sync::before': {
+          content: '"\\e954"',
+        },
+
+        '.icon-telephone::before': {
+          content: '"\\e955"',
+        },
+
+        '.icon-thumb-filled::before': {
+          content: '"\\e956"',
+        },
+
+        '.icon-thumb::before': {
+          content: '"\\e957"',
+        },
+
+        '.icon-eyedropper::before': {
+          content: '"\\e958"',
+        },
+      },
+      variants('icon')
+    )
+  }
+}
