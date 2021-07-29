19e40229da03096cc39e33247d7b6a252bfb6479
wengYuting <wengyuting3@gmail.com>
2021-07-29 14:03:54 +0800

    [修復] 滿版 Loading 在 IE 11 定位偏移的問題

--- src/plugins/guideline/cLoading.js
+++ src/plugins/guideline/cLoading.js
@@ -25,12 +25,14 @@ export default function() {
         'position': 'absolute',
         'top': '0',
         'left': '0',
-        'display': 'flex',
-        'align-items': 'center',
-        'justify-content': 'center',
         'width': '100%',
         'height': '100%',
-        'background-color': 'rgba(41, 41, 41, .16)'
+        'background-color': 'rgba(41, 41, 41, .16)',
+        '&.loading::after' :{
+          'position': 'absolute',
+          'top': 'calc(50% - 18px)',
+          'left': 'calc(50% - 18px)',
+        }
       }
     },
     variants('cLoading')
